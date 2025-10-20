e910acea24cbaf3088fb3810162afd43137c9176
Zifeng Li <zifengli114@gmail.com>
2025-10-20 16:48:55 -0400

    refactored

--- script3/short_cut.py
+++ script3/short_cut.py
@@ -1,6 +1,6 @@
 import os,pathlib,subprocess,re
 from pathlib import Path
-#Zifeng Li, 10/16/2025
+#Zifeng Li, 10/20/2025
 def menu():
     print("[1] Create a symbolic link")
     print("[2] Delete a symbolic link")
@@ -23,20 +23,25 @@ def createSymbolicLink():
         for match in matches:
             print(index,match[index])
             index+=1
+        try:
+            choice = int(input("Which file do you want to creata symbolic link for(index): "))
+            if choice >= len(matches) or choice < 0:
+                print("Choice out of range")
+                return 
+            else:
+                path = matches[choice]
+        except:
+            print("Bad input")
+            return
+    elif len(matches)==1:
+        path = matches[0]
     elif not matches:
         print("File doesn't exist")
         return
     
     #safeguard user input
-    try:
-        choice = int(input("Which file do you want to creata symbolic link for(index): "))
-    except:
-        print("Bad input")
-        return
-    if choice >= len(matches) or choice < 0:
-        print("Choice out of range")
-        return 
-    path = matches[choice]
+    
+    
 
     if path != "":#safeguard against not finding a path
         cmd=["ln", "-s", path, f"{str(Path.home())}/{name}"]#establishes a symbolics link for the desginated file
@@ -50,7 +55,7 @@ def deleteSymbolicLink():
     os.system("clear")
     name = input("Enter the file name: ")
     link = Path(f"{str(Path.home())}/{name}")
-    if not link.is_symlink():
+    if not link.is_symlink():#checks if the file is a symbolic link or not
         print(f"{link} doesn't exist or is not a symbolic link.")
         return
     #deletes the symbolic file for the specified file
@@ -64,16 +69,12 @@ def symbolicLinkReport():
     #requirement: #of links, links and their target paths
     os.chdir(Path.home())
     cmd = ["find", ".", "-type", "l", "-printf", "link:%p -> target:%l\n"]
-    process = subprocess.run(cmd,text=True, capture_output=True, check=True)#formats all symbolic links into a file
+    process = subprocess.run(cmd,text=True, capture_output=True, check=True)#formats all symbolic links 
     # Print lines and show count
-    lines = [line for line in process.stdout.splitlines() if line.strip()]
+    lines = [line for line in process.stdout.splitlines() if line.strip()]#splits the output into lines
     for line in lines:
         print(line)
     print(f"\nTotal count of symbolic links: {len(lines)}")
-
-
-
-
 
 def main():
     os.system("clear")
