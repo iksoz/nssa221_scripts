2bc194ce7cd0bcbcadc0e9747ccefb3503c7eb09
Zifeng Li <zifengli114@gmail.com>
2025-11-03 13:54:40 -0500

    attempt to find all ips

--- script4/attacker_report.py
+++ script4/attacker_report.py
@@ -1,15 +1,27 @@
 #Name:Zifeng Li
 #11/03/2025
-import os,subprocess,pathlib
+import os,subprocess,pathlib,re
 from pathlib import Path
 #,geoip
 #from geoip import geolite2 
 
 def report_gen():
+    #locates and opens the syslog file
     os.chdir(Path.home())
     os.chdir("student")
-    stdoutput = subprocess.run(["pwd"],text=True,capture_output=True)
-    print(stdoutput.stdout)
+    syslog = open("syslog.log")
+    ips = []
+    for line in syslog:
+        part = line.split("")
+        matching = re.findall(r"(?:\d{1,3}\.){3}\d{1,3}",line)
+        if matching not in ips:
+            ips+= matching
+        elif matching in ips:
+            continue
+
+    syslog.close()
+
+    print(ips)
 
 def main():
     os.system("clear")
