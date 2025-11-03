bce7a3e459c4f1a6ae66358d131426d52bb3f426
Student <student@localhost.localdomain>
2025-11-03 15:28:55 -0500

    finished sorting

--- script4/attacker_report.py
+++ script4/attacker_report.py
@@ -1,5 +1,6 @@
 #Name:Zifeng Li
 #11/03/2025
+#! can be ran anywhere
 import os,subprocess,pathlib,re,geoip
 from pathlib import Path
 from geoip import geolite2 
@@ -10,30 +11,35 @@ def report_gen():
     os.chdir("student")
     syslog = open("syslog.log")
 
-    ips = set()
     count = {}
     ip_pattern = re.compile(r'\b(?:\d{1,3}\.){3}\d{1,3}\b')#match IPs
 
     for line in syslog:
         matching = ip_pattern.findall(line)#find all IPs in line
-        if len(matching) == 0:
+        if len(matching) == 0:#continues if the line in log did not contain an ip
             continue
         for ip in matching:#add all ips found in the line into a stored set and dictionary
-            ips.add(ip)
             count[ip] = count.get(ip,0) + 1
     syslog.close()  
 
     #searches for the country associated with the ip
     ip_counter_country = []
     for ip,counter in count.items():
-        countryFound = geolite2.lookup(ip)
-        if countryFound:
-            ip_counter_country += [counter,ip,countryFound]
+        search = geolite2.lookup(ip)
+        if search and counter >= 10:#if a country can be found associated with the ip and has more than 10 failed attempts
+            countryFound = search.country#match the country
+            ip_counter_country.append((counter,ip,countryFound))#formats it for output
+
+    #sorts the list of entries
+    ip_counter_country.sort()
 
     #format report
-    print("COUNT","\tIP ADDRESS","\tCOUNTRY")
+    stdoutput = subprocess.run(["date"],text=True,capture_output=True)
+    print(stdoutput.stdout)
+    print("COUNT","\tIP ADDRESS","\tCOUNTRY",sep="")
+
     for i in ip_counter_country:
-        print(str(i[0]))
+        print(i[0],'\t',i[1],'\t',i[2])
 
 def main():
     os.system("clear")
