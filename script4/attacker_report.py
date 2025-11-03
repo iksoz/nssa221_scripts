530575be638a740141fe00387c00b6266fa3fbfd
Zifeng Li <zifengli114@gmail.com>
2025-11-03 14:54:33 -0500

    (testing)finds country matching with ip

--- script4/attacker_report.py
+++ script4/attacker_report.py
@@ -26,7 +26,7 @@ def report_gen():
     #searches for the country associated with the ip
     ip_counter_country = []
     for ip,counter in count.items():
-        country = geolite2.lookup(ip)
+        country = geolite2.lookup(ip).country
         if country:
             ip_counter_country += [counter,ip,country]
 
