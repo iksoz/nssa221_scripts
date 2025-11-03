8bc2ba4c6826b7601a2820695f7dd8bf359e4258
Zifeng Li <zifengli114@gmail.com>
2025-11-03 13:33:37 -0500

    1

--- script4/attacker_report.py
+++ script4/attacker_report.py
@@ -1,5 +1,18 @@
-import os,subprocess
+#Name:Zifeng Li
+#11/03/2025
+import os,subprocess,pathlib
+from pathlib import Path
+#,geoip
+#from geoip import geolite2 
 
-def 
+def report_gen():
+    os.chdir(Path.home()+"/student")
+    stdoutput = subprocess.run(["pwd"],text=True,capture_output=True)
+    print(stdoutput)
 
 def main():
+    os.system("clear")
+    report_gen()
+
+if __name__ == "__main__":
+    main()
